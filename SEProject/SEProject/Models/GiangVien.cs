b72f5d1fabb76e7dcc59417b6a8617d778c93823
TienDao <thiendao.online@gmail.com>
2013-04-05 07:55:45 +0700

    add class MonHoc, GiangVIen

---
+++ SEProject/SEProject/Models/GiangVien.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace SEProject.Models
+{
+    public class GiangVien
+    {
+        public int Id { get; set; }
+        public string hoTen { get; set; }
+        public string chucDanh { get; set; }
+        public string chuyenNganh { get; set; }
+        public string donVi { get; set; }
+
+    }
+
+    public class GiangVienDBContext : DbContext
+    {
+        public DbSet<GiangVien> giangViens { get; set; }
+    }
+}
---
+++ SEProject/SEProject/Models/MonHoc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace SEProject.Models
+{
+    public class MonHoc
+    {
+        public int Id {get; set;}
+        public string maMonHoc { get; set; }
+        public string tenMonHoc {get; set;}
+        public int soTinChi { get; set; }
+        public int gioLyThuyet { get; set; }
+        public int gioThucHanh { get; set; }
+        public int gioTuHoc { get; set; }
+        public string maMonKienQuyet { get; set; }
+    }
+
+    public class MonHocDBContext : DbContext
+    {
+        public DbSet<MonHoc> monHocs { get; set; }
+    }
+}
